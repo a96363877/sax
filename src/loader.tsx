063b61d55684921e3e8b656b7c2e3bb265cd6a62
a96363877 <a96363877@gmail.com>
2025-01-31 16:34:16 -0800

    sfd

--- src/loader.tsx
+++ src/loader.tsx
@@ -3,7 +3,6 @@ import { TailSpin } from "react-loader-spinner"
 export const Loader = (props:{show:boolean}) => {
     return (<div style={{display:props.show?"flex":"none",position:'fixed',
     flexDirection:"column",textAlign:'center',justifyItems:"center",justifyContent:'center',top:'45%',bottom:'auto',left:0,right:0,zIndex:1}}>
-        
         <TailSpin
             visible={true}
             height="80"
